cf52a1fd7fb3c293caacb0f0a5ca8f7081b2010f
akafael <raffaellimma@gmail.com>
2013-07-12 11:33:02 -0300

    Reading Datafile

--- curve_fitting/src/polynomial.py
+++ curve_fitting/src/polynomial.py
@@ -1,13 +1,66 @@
+#! /usr/bin/env python
 """
 
-
-@autor Diógenes
-@autor Rafael Lima
+autors Diogenes, Felipe , Rafael Lima
 
 """
 # import numpy as np
+import sys, os, math
+#from fractions import Fraction
+
 
 # read data of table1.dat:
-f = open('../data/table1.dat','r')
+def lerMatriz():
+	matriz = [] 
+	# contador
+	i = 0
+	#dimensoes da matriz
+	linhas = 0
+	colunas = 0
+
+	arquivo = open('../data/table1.dat','r')
+	print str(file)
+	print str('\n')
+
+	#Trasforma o arquivo lido em uma matriz 2D
+	for linha in arquivo:
+		linha = linha.strip().split('\t')
+		matriz.append([])
+		matriz[i] = linha
+		i += 1
+
+	linhas = len(matriz)
+
+	for c in matriz[0]:
+		colunas += 1
+
+	arquivo.close()
+
+	return (matriz,linha,colunas)
+
+# print matrix:
+def imprimeMatriz(matriz):
+	for i in range(len(matriz)):
+		for j in range(len(matriz[i])):
+			print str(matriz[i][j])
+		print # pula uma linha
+
+
+# testando funcoes:
+# TODO Verificar se funciona
+matriz = lerMatriz()
+imprimeMatriz(matriz)
+
+
+# Ajuste polinomial
+"""
+1 definir matrizes A , transposta de A e Y
+2 Calcular os produtos de matrizes A*AT e AT*Y
+3 Resolver o sistema.
+"""
+
+
 
 # do polymonial fitting:
+
+
